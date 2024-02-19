9a30f9738f759abda543e7302083312a4daac895
Miguel-Paredes <os.miguelparedes@gmail.com>
2024-02-19 16:31:10 -0500

    update

--- fronted-veterinaria/src/paginas/Listar.jsx
+++ fronted-veterinaria/src/paginas/Listar.jsx
@@ -4,9 +4,9 @@ import Tabla from '../componets/Tabla'
 const Listar = () => {
     return (
         <div>
-            <h1 className='font-black text-4xl text-gray-500'>Listar...</h1>
+            <h1 className='font-black text-4xl text-gray-500'>Pacientes</h1>
             <hr className='my-4' />
-            <p className='mb-8'>Este módulo te permite registrar listar .....</p>
+            <p className='mb-8'>Este módulo te permite listar los pacientes registrados</p>
             <Tabla/>
         </div>
     )
