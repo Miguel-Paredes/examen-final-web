52c623286e3b7769f87cb1a412755c6a90021d66
Alejandro Moreira <117743484+Alejandro-Moreira@users.noreply.github.com>
2024-03-04 10:01:45 -0500

    Update server.js

--- api-veterinaria-tratamientos/src/server.js
+++ api-veterinaria-tratamientos/src/server.js
@@ -21,16 +21,14 @@ const app = express()
 dotenv.config()
 
 // Configuraciones 
+app.use(cors({origin: '*'
+optionsSuccessStatus: 200}))
 app.set('port',process.env.port || 3000)
-app.use(cors())
 
 // Middlewares 
 app.use(express.json())
 
-
 // Variables globales
-
-
 
 // Rutas 
 app.use('/api',routerVeterinarios)
